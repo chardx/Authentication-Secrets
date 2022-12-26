1c65d05c664a4a330795e1addb04aeef2880f449
Richard Roxas <chardsourcex@gmail.com>
2022-12-26 16:56:07 +0800

    callback URL changes

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://elegant-shirt-deer.cyclic.app/auth/google/secrets",
 
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
@@ -112,7 +112,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    callbackURL: "https://elegant-shirt-deer.cyclic.app/auth/facebook/secrets",
     // profileFields: ['id', 'displayName', 'photos', 'email']
 },
     function (accessToken, refreshToken, profile, cb) {
